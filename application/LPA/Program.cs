74c3b958d799ac14a65bfa1b3d34490007c1795e
MutatedGamer <mutatedgamerxl@gmail.com>
2022-02-04 20:45:34 -0800

    Initial commit

---
+++ application/LPA/Program.cs
@@ -0,0 +1,28 @@
+using ElectronNET.API;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+
+builder.WebHost.UseElectron(args);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html");
+
+Task.Run(async () => await Electron.WindowManager.CreateWindowAsync());
+app.Run();
